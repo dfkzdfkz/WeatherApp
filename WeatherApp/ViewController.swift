96e1180b074af14614cd6afe41491d0633a6c95c
Valentina Abramova <valentisha@gmail.com>
2019-09-10 16:15:12 +0300

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Valentina Abramova on 10/09/2019.
+//  Copyright © 2019 Valentina Abramova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
