896a02b21ce979e00acb7c8ead250c5c4d62881d
Valentina Abramova <valentisha@gmail.com>
2019-09-20 13:40:27 +0300

    composite URL

--- WeatherApp/APIWeatherManager.swift
+++ WeatherApp/APIWeatherManager.swift
@@ -8,6 +8,31 @@
 
 import Foundation
 
+struct Coordinates {
+    let latitude: Double
+    let longitude: Double
+}
+
+enum ForecastType: FinalURLPoint {
+    case Current(apiKey: String, coordinates: Coordinates)
+    
+    var baseURL: URL {
+        return URL(string: "https://api.darksky.net")!
+    }
+    
+    var path: String {
+        switch self {
+        case .Current(let apiKey, let coordinates):
+            return "/forecast/\(apiKey)/\(coordinates.latitude),\(coordinates.longitude)"
+        }
+    }
+    
+    var request: URLRequest {
+        let url = URL(string: path, relativeTo: baseURL)
+        return URLRequest(url: url!)
+    }
+}
+
 final class APIWeatherManager: APIManager {
     
     let sessionConfiguration: URLSessionConfiguration
@@ -25,6 +50,10 @@ final class APIWeatherManager: APIManager {
     
     convenience init(apiKey: String) {
         self.init(sessionConfiguration: URLSessionConfiguration.default, apiKey: apiKey)
+    }
+    
+    func fetchCurrentWeatherWith(coordinates: Coordinates, completionHandler: (APIResult<CurrentWeather>) -> Void) {
+        let request = ForecastType.Current(apiKey: self.apiKey, coordinates: coordinates)
     }
     
 }
